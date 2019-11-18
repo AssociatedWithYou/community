885b609ed753628f695298906ffc0aca397b1a23
jiane <Associated_zp@163.com>
2019-11-18 21:28:25 +0800

    login ok

--- src/main/java/com/jiane/controller/OAuthorizeController.java
+++ src/main/java/com/jiane/controller/OAuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class OAuthorizeController {
 
@@ -27,7 +29,8 @@ public class OAuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code,
-                           @RequestParam("state") String state) {
+                           @RequestParam("state") String state,
+                           HttpSession session) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(client_id);//申请的github的Oauthor app的id
         accessTokenDTO.setClient_secret(client_secret);//申请的github的oauthor app的secret
@@ -36,7 +39,14 @@ public class OAuthorizeController {
         accessTokenDTO.setState(state);
         String accessTokenByGithub = githubProvider.getAccessTokenByGithub(accessTokenDTO);
         GithubUser githubUser = githubProvider.getGithubUser(accessTokenByGithub);
-        System.out.println(githubUser.getId()+"___"+githubUser.getName()+"___"+githubUser.getBio());
-        return "index";
+//        System.out.println(githubUser.getId()+"___"+githubUser.getName()+"___"+githubUser.getBio());
+        if (githubUser == null) {
+//            登陆失败
+            return "redirect:index";
+        }else{
+//            登陆成功
+            session.setAttribute("githubUser",githubUser);
+            return "redirect:index";
+        }
     }
 }
